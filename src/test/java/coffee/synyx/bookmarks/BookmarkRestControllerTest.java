50c61f1630cf7483f8a297ce175d59c5db38550f
Robin de Silva Jayasinghe <robin@jayasinghe.de>
2016-02-26 15:21:27 +0100

    optimized imports

--- src/test/java/coffee/synyx/bookmarks/BookmarkRestControllerTest.java
+++ src/test/java/coffee/synyx/bookmarks/BookmarkRestControllerTest.java
@@ -15,13 +15,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.mock.http.MockHttpOutputMessage;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
